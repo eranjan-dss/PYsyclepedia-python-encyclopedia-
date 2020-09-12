bfa77626e89862a51e6226aed44ba0bc78323901
eranjan <sunethra8023@gmail.com>
2020-09-12 09:54:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def click():
+    hello = input("say start")
+    if (hello == "start"):
+        print("hello")
+        vartype = input("what do you need?")
+        if(vartype == "owls"):
+          pop = """
+          type:animal
+          name:bird
+          bird type:owl
+          """
+          print(pop)
+click()
